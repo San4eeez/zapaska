6a467a27a389af5617165b6d5d0d3e145b9db184
San4eeez <AlexandroHell@yandex.ru>
2023-10-04 00:09:27 +0300

    Добавьте файлы проекта.

---
+++ zapaska/ClassyMetody.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ClassyMetody
+{
+    internal class CarAuto
+    {
+        private string number;
+        private float fuel;
+        private float flow;
+        private float currentDistance = 0; //для текущего пройденного пути
+
+        public void info(string number, float fuel, float flow)
+        {
+            this.number = number;
+            this.fuel = fuel;
+            this.flow = flow;
+        }
+
+        public void outinfo()
+        {
+            Console.WriteLine($"Номер: {this.number}\nТопливо: {this.fuel}\nРасход: {this.flow}");
+        }
+
+        private void zaprawka(float top)
+        {
+            this.fuel += top;
+        }
+
+        public void Probeg(float probeg)
+        {
+            this.currentDistance += probeg; // Увеличиваем текущий  путь
+        }
+
+        public void OutProbeg()
+        {
+            Console.WriteLine($"Текущий пробег: {this.currentDistance} км");
+        }
+
+        public void move(int km)
+        {
+            float TempFlow = this.flow;
+            float distance = km;
+
+            float ostatok = this.fuel - (distance * (this.flow) / 100);
+            if (ostatok >= 0)
+            {
+                Console.WriteLine("Осталось в баке: " + ostatok + " л");
+                Console.WriteLine("Ты доехал!");
+                this.currentDistance += distance; // Увеличиваем текущий пройденный путь
+            }
+            else
+            {
+                Console.WriteLine("А ты хрен доедешь, давай зальём?");
+                zaprawka(float.Parse(Console.ReadLine()));
+                ostatok = this.fuel - (distance * (this.flow) / 100);
+
+                if (ostatok >= 0)
+                {
+                    Console.WriteLine("Мы смогли доехать до рая и в баке осталось: " + ostatok);
+                    this.currentDistance += distance; // Увеличиваем текущий путь
+                }
+                else
+                {
+                    float for_ostatok = (ostatok * -1);
+                    Console.WriteLine($"Вам не хватило топлива до рая. Нужно было залить ещё {for_ostatok:F1} литра(ов)");
+                }
+            }
+        }
+
+        public float GetTotalDistance()
+        {
+            return this.currentDistance; //возврат общего пути
+        }
+    }
+}
---
+++ zapaska/Program.cs
@@ -0,0 +1,76 @@
+﻿using ClassyMetody;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество топлива в баке: ");
+        float topliv = float.Parse(Console.ReadLine());
+        while (true)
+        {
+
+
+            Console.WriteLine("При начальной скорости 60 км/ч - расход 10 л/100 км.");
+            Console.WriteLine("Разгонимся или притормозим? (для разгона положительное число, для торможения - отрицательное.)");
+            float speedChange = float.Parse(Console.ReadLine());//разгон
+
+
+            float speed = 60;//эталон
+            float rashod = 10;
+
+            if (speedChange > 0)
+            {
+                // Увеличиваем скорость
+                while (speed < (60 + speedChange))
+                {
+                    speed += 1;
+                    rashod += 0.1F;
+                    Console.Clear();
+                    Console.WriteLine($"Скорость: {speed}");
+                    System.Threading.Thread.Sleep(180);
+                }
+            }
+            else if (speedChange < 0)
+            {
+                // Уменьшаем скорость
+                while (speed > (60 + speedChange))
+                {
+                    if (speed <= 1)
+                    {
+                        Console.WriteLine("Дружок - пирожок, ты что-то напутал в торможении. Нельзя ехать меньше 1. Считай что ты это и ввёл");
+                        speed = 0.1F;
+                        rashod = 4;
+                        break;
+                    }
+                    else
+                    {
+                        speed -= 1;
+                        rashod -= 0.1F;
+                        Console.Clear();
+                        Console.WriteLine($"Скорость: {speed}");
+                        System.Threading.Thread.Sleep(10);
+                    }
+                }
+            }
+
+            CarAuto carAuto = new CarAuto();
+            carAuto.info("п248он", topliv, rashod);
+            carAuto.outinfo();
+
+            Random random = new Random();
+            int a = random.Next(1, 200);
+            Console.WriteLine($"Наш путь до кабинета 1.11: {a} км (ДАЛЬШЕ БОГА НЕТ)");
+            carAuto.move(km: a);
+            Console.WriteLine("Приехали. Едем дальше? (да нет)");
+            string edem = Console.ReadLine();
+            carAuto.Probeg(a);
+            carAuto.OutProbeg();
+            if (edem == "нет")
+            {
+                Console.WriteLine($"Общий пробег: {carAuto.GetTotalDistance()} км");
+                break;
+            }
+        }
+    }
+}
